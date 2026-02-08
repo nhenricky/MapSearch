d2ffcdda7e79da303c057ce4033c772aaccb13ed
Natã Henrique <nathanhenri127@gmail.com>
2026-02-08 03:05:34 -0300

    Update map.py

--- map.py
+++ map.py
@@ -1,48 +1,59 @@
 from nicegui import ui
+import requests
 
-ui.label('MapSearch').classes(
-    'text-4xl font-extrabold text-center mt-6 '
-    'bg-gradient-to-r from-purple-600 via-green-500 to-black-600 '
-    'text-transparent bg-clip-text '
-    'animate-pulse '
-)
-ui.separator()
-
-CONTINENT_CENTERS = {
-    'North America': ((54.5260, -105.2551), 3),
-    'South America': ((-8.7832, -55.4915), 3),
-    'Europe': ((54.5260, 15.2551), 4),
-    'Africa': ((-8.7832, 34.5085), 3),
-    'Asia': ((34.0479, 100.6197), 3),
-    'Oceania': ((-22.7359, 140.0188), 4),
-    'Antarctica': ((-82.8628, 135.0000), 2),
-}
-
-def move_map(e):
-    if e.value is None:
+def search_location():
+    query = search_input.value
+    if not query:
         return
-    center, zoom = CONTINENT_CENTERS[e.value]
-    map.center = center
-    map.zoom = zoom 
 
-with ui.row().classes('justify-center items-center gap-8 mt-6'):
-    map = ui.leaflet(
-        center=(51.505, -0.09),
-        zoom=5).classes('w-[300px] h-[300px] rounded-x1 shadow-lg')
-        
-    with ui.column().classes('gap-4'):
-        
-        ui.label('Select a Continent').classes(
-            'text-lg font-semibold text-gray-800'
-        )
-        ui.select(
-            options=list(CONTINENT_CENTERS.keys()),
-            label='Continent',
-            on_change=move_map
-        ).classes(
-            'w-56 text-base font-medium '
-            'border-2 border-purple-600 '
-            'rounded-lg shadow-md bg-white'
-        )
+    url = 'https://nominatim.openstreetmap.org/search'
+    params = {
+        'q': query,
+        'format': 'json',
+        'limit': 1
+    }
+    headers = {
+        'User-Agent': 'MapSearchApp/1.0'
+    }
+
+    response = requests.get(url, params=params, headers=headers)
+    data = response.json()
+
+    if data:
+        lat = float(data[0]['lat'])
+        lon = float(data[0]['lon'])
+
+        map.center = (lat, lon)
+        map.zoom = 10
+
+        ui.notify(f'📍 {data[0]["display_name"]}')
+    else:
+        ui.notify('❌ Location not found', color='negative')
+
+
+map = ui.leaflet(center=(0, 0), zoom=2).style(
+    'position: fixed; top: 0; left: 0; width: 100vw; height: 100vh; z-index: 0;'
+)
+
+
+ui.dark_mode().enable()
+with ui.card().style(
+    'position: fixed; '
+    'top: 16px; '
+    'right: 16px; '
+    'z-index: 10; '
+    'width: 100%; '
+    'max-width: 300px;'
+):
+   ui.markdown('**MapSearch**').classes(
+        'text-2xl'
+    )
+
+   search_input = ui.input(
+        placeholder='Search city or country...'
+    ).props('outlined dense').classes('w-full')
+
+   ui.button('Search', on_click=search_location).classes('w-full mt-2')
+
 
 ui.run()
